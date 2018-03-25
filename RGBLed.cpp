249a7a987fb14bdab06a937919593abc045b6f88
jpinon <github@jean.pinon.name>
2018-03-25 18:57:03 +0200

    v1

---
+++ RGBLed.cpp
@@ -0,0 +1,29 @@
+#include "RGBLed.h"
+
+RgbLed::RgbLed(int redPin, int greenPin, int bluePin){
+    _redPin=redPin;       pinMode(_redPin,OUTPUT);
+    _greenPin=greenPin;   pinMode(_greenPin,OUTPUT);
+    _bluePin=bluePin;     pinMode(_bluePin,OUTPUT);
+  // initial value
+    set(HIGH,HIGH,HIGH);
+    updateLeds();
+}
+
+
+void RgbLed::set(int r, int g, int b){
+    int _red=r;
+    int _green=g;
+    int _blue=b;      
+}
+
+void RgbLed::updateLeds(){
+  digitalWrite(_redPin,_red);
+  digitalWrite(_greenPin,_green);
+  digitalWrite(_bluePin,_blue);  
+}
+
+void RgbLed::green(){
+    set(LOW,HIGH,LOW);
+    updateLeds();  
+}
+
---
+++ RGBLed.h
@@ -0,0 +1,22 @@
+#ifndef RGBLED
+ #define RGBLED
+ #include <stdint.h>
+ #include <Arduino.h>
+  
+ class RgbLed {
+  public:
+    RgbLed(int redPin, int greenPin, int bluePin);
+    void set(int r, int g, int b);
+    void updateLeds();
+    void green();
+  private:
+  // Connections
+    int _redPin;
+    int _greenPin;
+    int _bluePin;
+  // light value
+    int _red;
+    int _green;
+    int _blue;
+ };
+#endif
